0bea38d76b9c0dc22f44ed0aba8b13752066898a
philip howley <howley.phil@gmail.com>
2022-05-20 23:00:47 -0400

    Add ternary examples

---
+++ script.js
@@ -0,0 +1,34 @@
+// condition ? doThisIfTrue : doThisIfFalse
+
+1 > 2 ? console.log(true) : console.log(false)
+// returns false
+
+// using if else
+// let age = 18;
+// let message;
+//
+// if (age >= 16) {
+//   message = 'You can drive.';
+// } else {
+//   message = 'You cannot drive.';
+// }
+//
+// console.log(message);
+
+// using ternary
+// let age = 18;
+// let message;
+//
+// age >= 16 ? (message = 'You can drive.') : (message = 'You cannot drive.');
+//
+// console.log(message);
+
+// use ternary in expression
+// let age = 18;
+// let message;
+//
+// message = age >= 16 ? 'You can drive.' : 'You cannot drive.';
+//
+// console.log(message);
+//
+// [source](https://www.javascripttutorial.net/javascript-ternary-operator/)
