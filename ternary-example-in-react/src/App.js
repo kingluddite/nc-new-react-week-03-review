fab3b2e464bacf7e2162dfe6607a60db247586c1
philip howley <howley.phil@gmail.com>
2022-05-20 23:06:41 -0400

    Add code for ternary in react

---
+++ ternary-example-in-react/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [toggle, setToggle] = useState(false)
+
+  const toggleFunc = () => {
+    setToggle(!toggle)
+  }
+
+  return (
+    <div className={toggle ? 'container salmon' : 'container'}>
+      <button className='btn' onClick={toggleFunc}>
+        Toggle
+      </button>
+    </div>
+  )
+}
+
+export default App
